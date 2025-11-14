2df4d67b3c0b9e0ddc3df7dc80ec1372071eeb53
rclarke <rclarke009@gmail.com>
2025-11-14 11:27:29 -0500

    Refactor sitemap generation to use Sanity for blog posts
    - Replaced the previous data fetching method with a query to Sanity to retrieve published blog posts.
    - Updated URL generation to reflect the new data structure, ensuring accurate last modified timestamps.
    - Removed deprecated GraphQL query and streamlined the sitemap entry creation process.

--- fresh-start-air-purifiers/app/routes/sitemap_blogs_1[.]xml.ts
+++ fresh-start-air-purifiers/app/routes/sitemap_blogs_1[.]xml.ts
@@ -1,18 +1,34 @@
 import type {LoaderFunctionArgs} from '@shopify/remix-oxygen';
+import {sanityClient} from '~/lib/sanityClient';
 
-export async function loader({context, request}: LoaderFunctionArgs) {
+export async function loader({request}: LoaderFunctionArgs) {
   const url = new URL(request.url);
   const baseUrl = `${url.protocol}//${url.host}`;
 
-  const data = await context.storefront.query(BLOGS_QUERY);
+  // Fetch all published blog posts from Sanity
+  const blogs = await sanityClient.fetch(`
+    *[_type == "blog" && defined(slug.current)] | order(publishedAt desc) {
+      "slug": slug.current,
+      "updatedAt": _updatedAt,
+      "publishedAt": publishedAt
+    }
+  `);
 
-  const urls = data.articles.nodes
-    .map(
-      (a) => `  <url>
-    <loc>${baseUrl}/blogs/${a.blog.handle}/${a.handle}</loc>
-    <lastmod>${a.updatedAt ? new Date(a.updatedAt).toISOString().split('T')[0] : new Date().toISOString().split('T')[0]}</lastmod>
-  </url>`,
-    )
+  // Generate URL entries for each blog post
+  const urls = blogs
+    .map((blog) => {
+      // Use publishedAt if available, otherwise use _updatedAt, otherwise today
+      const lastmod = blog.publishedAt 
+        ? new Date(blog.publishedAt).toISOString().split('T')[0]
+        : blog.updatedAt
+        ? new Date(blog.updatedAt).toISOString().split('T')[0]
+        : new Date().toISOString().split('T')[0];
+
+      return `  <url>
+    <loc>${baseUrl}/blogs/${blog.slug}</loc>
+    <lastmod>${lastmod}</lastmod>
+  </url>`;
+    })
     .join('\n');
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
@@ -27,18 +43,4 @@ ${urls}
     },
   });
 }
-
-const BLOGS_QUERY = `#graphql
-  query {
-    articles(first: 250, sortKey: UPDATED_AT) {
-      nodes {
-        handle
-        updatedAt
-        blog {
-          handle
-        }
-      }
-    }
-  }
-` as const;
 
